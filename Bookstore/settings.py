5c43258a5e15cd6318b3f14f2d8a6351d39b793a
Glory <nwaforglory6@gmail.com>
2022-09-06 17:51:55 +0100

    trying to fix reset password

--- Bookstore/settings.py
+++ Bookstore/settings.py
@@ -170,10 +170,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT=425
+EMAIL_PORT=578
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='nwaforglory6@gmail.com'
 EMAIL_HOST_PASSWORD="fkiorfsqwkkjzzch"
+DEFAULT_EMAIL="nwaforglory6@gmail.com"
 # EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
 
 print(EMAIL_HOST_PASSWORD,'+++')
